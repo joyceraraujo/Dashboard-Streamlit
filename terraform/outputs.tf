a555190c9414b7115c88ba5a9482e85c0755d2b3
joyceraraujo <joyceramosaraujo@gmail.com>
2021-08-06 15:06:03 +0200

    Edit output file to get url to directly access streamlit app

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "domain-name" {
   value = aws_instance.ec2-streamlit_tf.public_dns
 }
 
-# output "application-url" {
-#   value = "${aws_instance.ec2-streamlit_tf.public_dns}/index.php"
-# }
+output "application-url" {
+   value = "http://${aws_instance.ec2-streamlit_tf.public_dns}:8501"
+}
